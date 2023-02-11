563b33f2b60f6ec40449a4f5fd530db19a7a8e19
yoshi-non <himekawa.yoshino44@gmail.com>
2023-02-11 17:45:58 +0900

    ライブ配信している人のみ画像の透過度を上げて表示

--- app/src/main/java/com/example/youtubelivewidget/YoutubeLiveWidget.java
+++ app/src/main/java/com/example/youtubelivewidget/YoutubeLiveWidget.java
@@ -3,100 +3,109 @@ package com.example.youtubelivewidget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.RemoteViews;
 
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.YouTubeRequestInitializer;
-import com.google.api.services.youtube.model.Channel;
-import com.google.api.services.youtube.model.ChannelListResponse;
-import com.google.api.services.youtube.model.SearchListResponse;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Collections;
 
 public class YoutubeLiveWidget extends AppWidgetProvider {
 
-    private static class YoutubeAsyncTask extends AsyncTask<Void, Void, String> {
-        private final RemoteViews views;
-        private final AppWidgetManager appWidgetManager;
+    private static class YoutubeAsyncTask extends AsyncTask<Void, Void, Boolean> {
+
+        private String channelId;
+        private RemoteViews views;
+        private int imageViewId;
+        private Context context;
         private final int appWidgetId;
 
-        String ImageURL;
-
-        YoutubeAsyncTask(RemoteViews views, AppWidgetManager appWidgetManager, int appWidgetId) {
+        YoutubeAsyncTask(Context context, String channelId, RemoteViews views, int imageViewId, int appWidgetId) {
+            this.context = context;
             this.views = views;
-            this.appWidgetManager = appWidgetManager;
+            this.imageViewId = imageViewId;
+            this.channelId = channelId;
             this.appWidgetId = appWidgetId;
         }
 
         @Override
-        protected String doInBackground(Void... voids) {
+        protected Boolean doInBackground(Void... voids) {
+            Log.d("YoutubeLiveWidget", "doInBackground running");
+
+            //YoutubeAPIの初期設定
+            String API_KEY = BuildConfig.YOUTUBE_API_KEY;
+            String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + channelId + "&type=video&eventType=live&key=" + API_KEY;
+            Boolean channelIsLive = false;
+
+            //ライブ配信中かどうかを判断する
             try {
-                Log.d("YoutubeLiveWidget", "doInBackground running");
-                //YoutubeAPIの初期設定
-                String apiKey = BuildConfig.YOUTUBE_API_KEY;
-                YouTubeRequestInitializer initializer = new YouTubeRequestInitializer(apiKey);
-                YouTube youtube = new YouTube.Builder(new NetHttpTransport(), new GsonFactory(), request -> {}).setYouTubeRequestInitializer(initializer).setApplicationName("youtube-live-widget").build();
-                //特定のチャンネルのデータを取得
-                YouTube.Search.List search = youtube.search().list(Collections.singletonList("id,snippet"));
-                search.setType(Collections.singletonList("video"));
-                search.setEventType("live");
-                search.setMaxResults(1L);
-                search.setChannelId("UCFKOVgVbGmX65RxO3EtH3iw");
-                SearchListResponse searchResponse = search.execute();
-                //ライブ配信中かどうかを判断する
-                if (searchResponse.getPageInfo().getTotalResults() > 0) {
-                    //ライブ配信中なら配信者のアイコン画像を貼り付ける
-                    YouTube.Channels.List channelList = youtube.channels().list(Collections.singletonList("snippet"));
-                    channelList.setId(Collections.singletonList("UCFKOVgVbGmX65RxO3EtH3iw"));
-                    ChannelListResponse channelResponse = channelList.execute();
-                    Channel targetChannel = channelResponse.getItems().get(0);
-                    String urlText = targetChannel.getSnippet().getThumbnails().getHigh().getUrl();
-                    URL url = new URL(urlText);
-                    HttpURLConnection connection = null;
-                    try {
-                        connection = (HttpURLConnection) url.openConnection();
-                        connection.connect();
-                        views.setImageViewBitmap(R.id.imageView, BitmapFactory.decodeStream(connection.getInputStream()));
-                        appWidgetManager.updateAppWidget(appWidgetId, views);
-                    } catch (Exception e) {
-                        Log.d("YoutubeLiveWidget_ConnectApiError", e.toString());
-                        ImageURL = e.getStackTrace().toString();
-                    } finally {
-                        if (connection != null) {
-                            connection.disconnect();
-                        }
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    Log.d("response", "200");
+                    InputStream inputStream = connection.getInputStream();
+                    StringBuilder stringBuilder = new StringBuilder();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    JSONObject response = new JSONObject(stringBuilder.toString());
+                    int totalResults = response.getJSONObject("pageInfo").getInt("totalResults");
+                    if (totalResults > 0) {
+                        Log.d("YoutubeLiveWidget", "配信中");
+                        channelIsLive = true;
+                    } else {
+                        Log.d("YoutubeLiveWidget", "配信していません");
                     }
                 }
-                return "";
-            } catch (Exception e){
-
-                Log.d("YoutubeLiveWidget_Error", e.toString());
-                return ImageURL = e.getStackTrace().toString();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+            return channelIsLive;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean channelIsLive) {
+            super.onPostExecute(channelIsLive);
+//            Log.d("画像透過処理", "実行");
+            //ライブ配信中なら透過度を上げる
+            if (channelIsLive) {
+                views.setInt(imageViewId, "setAlpha", 255);
+            } else {
+                // 配信されていない場合は、背景色を白色にする
+                views.setInt(imageViewId, "setAlpha", 100);
+            }
+
+            // アプリウィジェットマネージャーを使用して更新する
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
-    void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_design);
-        new YoutubeAsyncTask(views, appWidgetManager, appWidgetId).execute();
-        Log.d("WIDGET", "Widget updated!");
-    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            try {
-                updateAppWidget(context, appWidgetManager, appWidgetId);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_design);
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_TokinoSora_id), views, R.id.imageView_01, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_Robocosan_id), views, R.id.imageView_02, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_YozoraMel_id), views, R.id.imageView_03, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_AkiRosenthal_id), views, R.id.imageView_04, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_AkaiHaato_id), views, R.id.imageView_05, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_ShirakamiFubuki_id), views, R.id.imageView_06, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_NatsuiroMatsuri_id), views, R.id.imageView_07, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_MinatoAqua_id), views, R.id.imageView_08, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_MurasakiShion_id), views, R.id.imageView_09, appWidgetId).execute();
+            new YoutubeAsyncTask(context, context.getResources().getString(R.string.liver_NakiriAyame_id), views, R.id.imageView_10, appWidgetId).execute();
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
